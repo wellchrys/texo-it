29893631917ba226e7ddd2c4f4a70ba38e911bc5
wellchrys <chrystiandejesus@hotmail.com>
2024-05-17 19:23:06 -0300

    Add a child process to start database

--- README.md
+++ README.md
@@ -26,12 +26,6 @@ Make sure the following ports will be available:
 - H2 Database: 5234
 - API: 3333
 
-Start the H2 Database with:
-
-```bash
-java -cp ./h2/bin/h2-2.2.224.jar org.h2.tools.Server -tcp -tcpAllowOthers -tcpPort 5234 -baseDir ./ -ifNotExists
-```
-
 Start Express endpoint with `yarn dev:server`
 
 Now you can visit [`localhost:3333`](http://localhost:3333/movie) from your browser.
--- src/index.js
+++ src/index.js
@@ -12,17 +12,36 @@ const { seeder, initialize } = require('./app/movie');
 // set port, listen for requests
 const PORT = process.env.PORT || 3333;
 
-server.listen(PORT, async () => {
-  await initialize();
+const { spawn } = require('child_process');
 
-  const jsonArray = await csv(
-    {
-      delimiter: ";",
-      headers: ["year", "title", "studios", "producers", "winner"]
-    }
-  ).fromFile("./src/movielist.csv");
+server.listen(PORT, () => {
+  const serverOptions = ['org.h2.tools.Server', '-tcp', '-tcpAllowOthers', '-tcpPort', '5234', '-baseDir', './', '-ifNotExists'];
 
-  await seeder(jsonArray);
+  const childProcess = spawn('java', ['-cp', './h2/bin/h2-2.2.224.jar', ...serverOptions]);
 
-  console.log(`> Server is running on port: ${PORT}.`);
+  childProcess.stdout.on('data', async (data) => {
+    console.log('H2 Database stdout:', data.toString());
+
+    await initialize();
+
+    const jsonArray = await csv(
+      {
+        delimiter: ";",
+        headers: ["year", "title", "studios", "producers", "winner"]
+      }
+    ).fromFile("./src/movielist.csv");
+
+    await seeder(jsonArray);
+
+    console.log(`> Server is running on port: ${PORT}.`);
+
+  });
+
+  childProcess.stderr.on('data', (data) => {
+    console.error('H2 Database stderr:', data.toString());
+  });
+
+  childProcess.on('close', (code) => {
+    console.log(`H2 Database child process exited with code ${code}`);
+  });
 });
